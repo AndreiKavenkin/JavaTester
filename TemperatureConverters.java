ef5536a3bc8c8bf578c403507b1856ac63688e81
AndreiKavenkin <bulbash1995@gmail.com>
2022-10-10 23:13:35 +0300

    Домашняя работа №8

---
+++ TemperatureConverters.java
@@ -0,0 +1,9 @@
+package Lesson8;
+
+public class TemperatureConverters {
+    public static double FahrenheitCelsius(double value) {
+        double result = (value - 32) / 1.8;
+        result = Math.round(result * 10);
+        return result / 10;
+    }
+}
